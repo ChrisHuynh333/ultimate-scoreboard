3b045b0fe0dc6154cc24d6ad74a242833394fa94
Chris <chris.huynh333@gmail.com>
2022-06-02 12:39:54 -0500

    Add undo button and some functionality. Still need to optimize undo logic

--- src/Score.js
+++ src/Score.js
@@ -6,8 +6,12 @@ const Score = () => {
     const {totalScore, setTotalScore, genderStatus, setGenderStatus, coed, halftime, firstPointGender, setHalftime} = useGlobalContext();
     const [genderCounter, setGenderCounter] = useState(0)
     const [pointsLog, setPointsLog] = useState([{points: totalScore, genderStatus: genderStatus}])
+    const [firstRender, setFirstRender] = useState(true)
+    const [totalPointsTracker, setTotalPointsTracker] = useState(0)
+    const [isUndo, setIsUndo] = useState(false)
 
     const changeScore = (score, index) => {
+        setIsUndo(false)
         const newScoreArray = [...totalScore]
         newScoreArray[index] = score + 1;
         setTotalScore(newScoreArray)
@@ -38,23 +42,41 @@ const Score = () => {
                 setGenderCounter(2);
             }
         }
+        if (!isUndo) {
+            setTotalPointsTracker(totalPointsTracker + 1)
+        }
+    }
+
+    const undoAction = () => {
+        setIsUndo(true)
+        console.log(totalPointsTracker)
+        setTotalPointsTracker(totalPointsTracker - 1)
+        console.log(totalPointsTracker)
+        setTotalScore(pointsLog[totalPointsTracker -1].points)
+        setGenderStatus(pointsLog[totalPointsTracker -1].genderStatus)
     }
     useEffect(() => {
         checkGender(genderCounter)
-        setPointsLog([...pointsLog, {points: totalScore, genderStatus}])
+        if(!firstRender) {
+            setPointsLog([...pointsLog, {points: totalScore, genderStatus}])
+        }
+        setFirstRender(false)
         console.log(pointsLog)
     },[totalScore])
     return (
-        <div className='score-container'>
-            {totalScore.map((score, index) => {
-                return (
-                    <div key={index}>
-                        <button onClick={() => changeScore(score, index)}>{score}</button>
-                    </div>
-                )
-            })}
-            {genderStatus.gender} {genderStatus.point}
-        </div>
+        <div>
+            <button onClick={() => undoAction()}>undo</button>
+            <div className='score-container'>
+                {totalScore.map((score, index) => {
+                    return (
+                        <div key={index}>
+                            <button onClick={() => changeScore(score, index)}>{score}</button>
+                        </div>
+                    )
+                })}
+                {genderStatus.gender} {genderStatus.point}
+            </div>
+            </div>
     )
 }
 
