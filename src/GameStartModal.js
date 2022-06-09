8b0cae78a282e46b3c3b11b873c55efcbceb04d3
Chris <chris.huynh333@gmail.com>
2022-06-09 15:49:54 -0500

    Finish GameStartModal styling

--- src/GameStartModal.js
+++ src/GameStartModal.js
@@ -28,44 +28,52 @@ const GameStartModal = () => {
     return (
         <div className={isGameStartModalOpen ? "game-start-modal-container" : "game-start-modal-container not-active"}>
             <form className="game-start-modal-form" onSubmit={handleSubmit}>
-                <div >
-                    <span>
-                        Gender Setting:
-                    </span>
-                    <span className="game-start-modal-radio-btn">
+                <div className="game-start-modal-row">
+                    <div className="game-start-modal-row-header">
+                        Gender Setting
+                    </div>
+                </div>
+                <div className="game-start-modal-row">
+                    <div className="game-start-modal-radio-btn">
                         <input type='radio' name="gender" id="coed" onClick={() => {setCoed(true); setTrackingGender(true)}} defaultChecked/>
                         <label for="coed">Coed</label>
                         <input type='radio' name="gender" id="singleGender" onClick={() => {setCoed(false); setTrackingGender(false)}} />
                         <label for="singleGender">Single Gender</label>
-                    </span>
+                    </div>
                 </div>
-                <div className={coed ? "active" : "not-active"}>
-                    <span>
-                        Track Gender Points: 
-                    </span>
-                    <span className="game-start-modal-radio-btn">
+                <div className={coed ? "game-start-modal-row" : "game-start-modal-row not-active"}>
+                    <div className="game-start-modal-row-header">
+                        Track Gender Points
+                    </div>
+                </div>
+                <div className={coed ? "game-start-modal-row" : "game-start-modal-row not-active"}>
+                    <div className="game-start-modal-radio-btn">
                         <input type='radio' name="trackGender" id="yesTrackGender" onClick={() => setTrackingGender(true)}defaultChecked/>
                         <label for="yesTrackGender">Yes</label>
                         <input type='radio' name="trackGender" id="noTrackGender" onClick={() => setTrackingGender(false)}/>
                         <label for="noTrackGender">No</label>
-                    </span>
+                    </div>
                 </div>
-                <div className={trackingGender ? "active" : "not-active"}>
-                    <span>
-                        Starting Gender: 
-                    </span>
-                    <span className="game-start-modal-radio-btn">
+                <div className={trackingGender ? "game-start-modal-row" : "game-start-modal-row not-active"}>
+                    <div className="game-start-modal-row-header">
+                        Starting Gender 
+                    </div>
+                </div>
+                <div className={trackingGender ? "game-start-modal-row" : "game-start-modal-row not-active"}>
+                    <div className="game-start-modal-radio-btn">
                         <input type='radio' name="startingGender" id="femaleStarting" value="female" onChange={e => handleGenderChange(e)} defaultChecked/>
                         <label for="femaleStarting">Female</label>
                         <input type='radio' name="startingGender" id="maleStarting" value="male" onChange={e => handleGenderChange(e)} />
                         <label for="maleStarting">Male</label>
-                    </span>
+                    </div>
                 </div>
-                <div>
-                    <span>
-                        Game Cap: 
-                    </span>
-                    <span className="game-start-modal-radio-btn">
+                <div className="game-start-modal-row">
+                    <div className="game-start-modal-row-header">
+                        Game Cap
+                    </div>
+                </div>
+                <div className="game-start-modal-row">
+                    <div className="game-start-modal-radio-btn">
                         <input type='radio' name="cap" id="capFifteen" value={15} onClick={() => {setCustomCap(false); setNoHalftime(false)}} onChange={e => handleScoreChange(e)} defaultChecked/>
                         <label for="capFifteen">15</label>
                         <input type='radio' name="cap" id="capThirteen" value={13} onClick={() => {setCustomCap(false); setNoHalftime(false)}} onChange={e => handleScoreChange(e)} />
@@ -74,32 +82,34 @@ const GameStartModal = () => {
                         <label for="capNoCap">No cap</label>
                         <input type='radio' name="cap" id="capCustomCap" onClick={() => {setCustomCap(true); setNoHalftime(false); setHalftimePoint(0)}} />
                         <label for="capCustomCap">Custom Cap</label>
-                    </span>
+                    </div>
                 </div>
-                <div className={customCap ? "active" : "not-active"}>
-                    <span>
-                        Custom Cap:
-                    </span>
-                    <span>
-                        <input type='number' onChange={e => handleScoreChange(e)} />
-                    </span>
+                <div className={customCap ? "game-start-modal-row" : "game-start-modal-row not-active"}>
+                    <div className="game-start-modal-row-header">
+                        <span>Custom Cap:  </span>
+                        <span>
+                            <input type='number' onChange={e => handleScoreChange(e)} />
+                            </span>
+                    </div>
                 </div>
-                <div>
-                    <span>
-                        Halftime Point:
-                    </span>
-                    <span className="game-start-modal-radio-btn">
+                <div className="game-start-modal-row">
+                    <div className="game-start-modal-row-header">
+                        Halftime Point
+                    </div>
+                </div>
+                <div className="game-start-modal-row">
+                    <div className="game-start-modal-radio-btn">
                         <input type='radio' name="halfTime" id="dynamicHalftimePoint" onClick={() => setNoHalftime(false)} checked={noHalftime ? null : true} />
                         <label for="dynamicHalftimePoint">{halftimePoint}</label>
                         <input type='radio' name="halfTime" id="noHalftime" onClick={() => setNoHalftime(true)} checked={noHalftime ? true : null}/>
                         <label for="noHalftime">No Halftime</label>
-                    </span>
+                    </div>
                 </div>
                 <div>
                     {customCapError ? "Please enter a points cap." : null}
                 </div>
-                <div>
-                    <button type="submit">Start Game</button>
+                <div className="game-start-modal-btn-container">
+                    <button className="game-start-modal-btn" type="submit">Start Game</button>
                 </div>
             </form>
         </div>
